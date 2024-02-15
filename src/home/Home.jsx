f0d33cc6a7e5f54958aa854f4fa8987192d8ad0f
Michelle Amina Okumu <michelleaokumu@gmail.com>
2024-02-15 06:55:51 +0300

    Commit completed home page

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,4 +1,8 @@
 import './home.css'
+import DiscountOutlinedIcon from '@mui/icons-material/DiscountOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import PaymentsOutlinedIcon from '@mui/icons-material/PaymentsOutlined';
+import PinDropOutlinedIcon from '@mui/icons-material/PinDropOutlined';
 
 
 
@@ -17,25 +21,26 @@ function Home(){
                 <div className="home-mid-text">
                 <h4>Latest Products</h4>
                 <h2>Discover New</h2>
+                <hr />
                 </div>
                 <div className="home-mid-images">
                     <div className="home-mid-content">
-                    <img src="public/1124edd94a94a25710b78f8393727388.jpg" alt="" />
+                    <img src="/1124edd94a94a25710b78f8393727388.jpg" alt="" />
                     <h3>Product name 1</h3>
                     <p>$300</p>
                     </div>
                     <div className="home-mid-content">
-                    <img src="public/c75b3fcdc3ddb736d1bd4451d8df6f7e.jpg" alt="" />
+                    <img src="/c75b3fcdc3ddb736d1bd4451d8df6f7e.jpg" alt="" />
                     <h3>Product name 2</h3>
                     <p>$700</p>
                     </div>
                     <div className="home-mid-content">
-                    <img src="public/beb7902ee08bf5db47dbd7bd7993611f.jpg" alt="" />
+                    <img src="/beb7902ee08bf5db47dbd7bd7993611f.jpg" alt="" />
                     <h3>Product name 3</h3>
                     <p>$1500</p>
                     </div>
                     <div className="home-mid-content">
-                    <img src="public/e34a65184e647e41997f2214f3c9328f.jpg" alt="" />
+                    <img src="/e34a65184e647e41997f2214f3c9328f.jpg" alt="" />
                     <h3>Product name 4</h3>
                     <p>$1000</p>
                     </div>
@@ -44,25 +49,26 @@ function Home(){
                 <div className="home-mid-text">
                 <h4>shop</h4>
                 <h2>Best selling</h2>
+                <hr />
                 </div>
                 <div className="home-mid-images">
                     <div className="home-mid-content">
-                    <img src="public/95efe514d75525cb308be7f1b0652fda.jpg" alt="" />
+                    <img src="/95efe514d75525cb308be7f1b0652fda.jpg" alt="" />
                     <h3>Product name 1</h3>
                     <p>$500</p>
                     </div>
                     <div className="home-mid-content">
-                    <img src="public/5ce57a153c88c23b5bb550faee4b86e2.jpg" alt="" />
+                    <img src="/5ce57a153c88c23b5bb550faee4b86e2.jpg" alt="" />
                     <h3>Product name 2</h3>
                     <p>$800</p>
                     </div>
                     <div className="home-mid-content">
-                    <img src="public/088ca799476956919b4716ec26d09b1d.jpg" alt="" />
+                    <img src="/088ca799476956919b4716ec26d09b1d.jpg" alt="" />
                     <h3>Product name 3</h3>
                     <p>$4000</p>
                     </div>
                     <div className="home-mid-content">
-                    <img src="public/8fef0e4f1e10a5be132c82ec51f79e9c.jpg" alt="" />
+                    <img src="/8fef0e4f1e10a5be132c82ec51f79e9c.jpg" alt="" />
                     <h3>Product name 4</h3>
                     <p>$2000</p>
                     </div>
@@ -71,36 +77,42 @@ function Home(){
             </div>
             <div className="home-lower">
                <div className="home-lower-text">
-               <h4>Unique Pieces Always On Trend</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+               <h4>Unique Pieces</h4>
+               <h2>Be Always On Trend</h2>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                </div>
                <div className='home-lower-right'>
-               <div className='home-small-right'>
-                    <img src="public/262b5c95135304500edb7a9fc95480c8.jpg" alt="" />
-                </div>
-                <img src="public/1363568210a045b3d06f5c4dec9f044a.jpg" alt="" />
+                   <img src="/1363568210a045b3d06f5c4dec9f044a.jpg" alt="" />
+                   <div className='home-small-right'>
+                        <img src="/Rings.jpeg" alt="" />   
+                    </div> 
                </div>
             </div>
-<div className="why-choose-us-container">
-            <h2>BEST IN BUSINESS</h2>
-            <h3>WHY CHOOSE US</h3>
+            <div className="why-choose-us-container">
+            <h4>Best In Business</h4>
+            <h2>Why Choose Us</h2>
             <p>CRAS MALESUADA DOLOR SIT AMET EST EGESTAS ULLAMCORPER. NULLAM IN TORTOR MI. MAECENAS VULPUTATE LIBERO</p>
-        </div>
-        <div className="promotion-container">
+            <hr />
+            </div>
+            <div className="promotion-container">
             <div className="promotion-card">
-                <h2>BIG DISCOUNTS</h2>
+                <DiscountOutlinedIcon className='promotion-icon'/>
+                <h2>Big Discounts</h2>
                 <p>Integer euismod blandit nunc sit amet sollicitudin. Fusce quis orci viverra, cursus justo.</p>
             </div>
             <div className="promotion-card">
-                <h2>FREE SHIPPING</h2>
+                <LocalShippingOutlinedIcon className='promotion-icon'/>
+                <h2>Free Shipping</h2>
                 <p>Integer euismod blandit nunc sit amet sollicitudin. Fusce quis orci viverra, cursus justo.</p>
             </div>
             <div className="promotion-card">
-                <h2>SECURE PAYMENTS</h2>
+                <PaymentsOutlinedIcon className='promotion-icon'/>
+                <h2>Secure Payments</h2>
                 <p>Integer euismod blandit nunc sit amet sollicitudin. Fusce quis orci viverra, cursus justo.</p>
             </div>
             <div className="promotion-card">
-                <h2>ORDER TRACKING</h2>
+                <PinDropOutlinedIcon className='promotion-icon'/>
+                <h2>Order Tracking</h2>
                 <p>Integer euismod blandit nunc sit amet sollicitudin. Fusce quis orci viverra, cursus justo.</p>
             </div>
         </div>
